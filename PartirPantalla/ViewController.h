6307a8ca03a787181461c56ed216ad60b4bc3e7f
Ruben Jeronimo Fernandez <rubenjeronimofernandez@MacBook-Pro-de-Ruben.local>
2014-07-08 14:39:32 +0200

    Initial Commit

---
+++ PartirPantalla/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PartirPantalla
+//
+//  Created by Ruben Jeronimo Fernandez on 08/07/14.
+//  Copyright (c) 2014 IronHack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ PartirPantalla/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PartirPantalla
+//
+//  Created by Ruben Jeronimo Fernandez on 08/07/14.
+//  Copyright (c) 2014 IronHack. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

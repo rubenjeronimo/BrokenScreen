8fe4f22b28ccbb323e282745d162af5f15b584ae
rubenjeronimo <ruben.jeronimo.fernandez@gmail.com>
2014-07-10 09:05:27 +0200

    broken screen

--- PartirPantalla/ViewController.h
+++ PartirPantalla/ViewController.h
@@ -9,5 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *imgIzquierda;
+@property (weak, nonatomic) IBOutlet UIImageView *imgDerecha;
 
 @end
--- PartirPantalla/ViewController.m
+++ PartirPantalla/ViewController.m
@@ -18,12 +18,58 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self reloadView];
+    
+
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)reloadView{
+    
+    UIDeviceOrientation orientacion = [[UIDevice currentDevice]orientation];
+    
+    if (orientacion==UIDeviceOrientationPortrait || orientacion==UIDeviceOrientationUnknown) {
+        [self setPortait];
+        
+    }else if (orientacion == UIDeviceOrientationLandscapeLeft || orientacion == UIDeviceOrientationLandscapeRight){
+        [self setLandscape];
+    }
+
+    
+
+}
+
+-(void)setPortait{
+
+    [self setLeftImage];
+    [self setRightImage];
+}
+
+-(void)setLandscape{
+    
+    [self setLeftImage];
+    [self setRightImage];
+}
+
+-(void) setLeftImage{
+    self.imgIzquierda.image = [UIImage imageNamed:@"sad-batman.jpeg"];
+    
+    
+    
+    
+
+
+}
+
+-(void) setRightImage{
+    self.imgDerecha.image = [UIImage imageNamed:@"sad-batman.jpeg"];
+
+
 }
 
 @end
